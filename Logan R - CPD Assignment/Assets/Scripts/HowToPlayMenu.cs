68d9df38cd7758d4c34cd964d759d73d4a3f4167
Logan Ryan <loganryan982@gmail.com>
2020-09-11 14:53:48 +1000

    Created How To Play Menu.

---
+++ Logan R - CPD Assignment/Assets/Scripts/HowToPlayMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HowToPlayMenu : MonoBehaviour
+{
+    private string[] controllers;
+
+    public GameObject mainMenu;
+    public GameObject startButton;
+    public GameObject keyboardControls;
+    public GameObject joystickControls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controllers = Input.GetJoystickNames();
+        Debug.Log(controllers.Length);
+
+        if (controllers.Length > 0)
+        {
+            keyboardControls.SetActive(false);
+            joystickControls.SetActive(true);
+        }
+        else
+        {
+            keyboardControls.SetActive(true);
+            joystickControls.SetActive(false);
+        }
+    }
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(startButton);
+        gameObject.SetActive(false);
+    }
+}
--- Logan R - CPD Assignment/Assets/Scripts/MainMenu.cs
+++ Logan R - CPD Assignment/Assets/Scripts/MainMenu.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject howToPlayMenu;
+    public GameObject startButton;
+    
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void HowToPlay()
+    {
+        howToPlayMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(startButton);
+        gameObject.SetActive(false);
     }
 
     public void QuitGame()
