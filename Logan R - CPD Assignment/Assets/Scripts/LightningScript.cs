12c7067fe3a8f04ce0ec89cf992f987809727a85
Logan Ryan <loganryan982@gmail.com>
2020-09-11 12:24:17 +1000

    Added lighting to the top part of the electric fence.

--- Logan R - CPD Assignment/Assets/Scripts/LightningScript.cs
+++ Logan R - CPD Assignment/Assets/Scripts/LightningScript.cs
@@ -16,7 +16,7 @@ public class LightningScript : MonoBehaviour
 
         if (timer >= waitTime)
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < lightningParticles.Length; i++)
             {
                 StartCoroutine(ShootLightning(i));
             }
